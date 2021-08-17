35f816c9ec595f2783f7e05f47d901c34a1baf60
juanluisja1 <jlja2@ensign.edu>
2021-08-17 09:44:12 -0600

    fix custom chat proyect

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,7 +1,7 @@
 const webSocketOutput = document.querySelector('.output');
 const inputBtn = document.querySelector('.input-box');
 
-inputBtn.addEventListener('keyup', function(event) {
+inputBtn.addEventListener('keypress', function(event) {
     if (event.key === "Enter") {
         const query = event.target.value;
         event.target.value = '';
